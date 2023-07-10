c3cba9e9151f601f49ae1c23dcb58b6bdebf24fe
Shikhar Agarwal <111875681+shikhar229169@users.noreply.github.com>
2023-07-10 19:20:57 +0530

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,7 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-verify");
+require('hardhat-gas-reporter');
+require("hardhat-deploy");
+require('@nomicfoundation/hardhat-ethers');
+require('hardhat-deploy-ethers');
+require('dotenv').config();
